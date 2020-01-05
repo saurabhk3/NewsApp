d22c8febb77d21f03aeb4c55b509b6e4e52a75fe
saurabhk3 <kannaujia944@gmail.com>
2020-01-06 00:26:22 +0530

    Content added to database and activity working

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -2,9 +2,15 @@ package com.example.newsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,23 +26,52 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> titles = new ArrayList<>();
+    ArrayList<String> contents = new ArrayList<>();
     ArrayAdapter arrayAdapter;
-
+    SQLiteDatabase articles;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        articles = this.openOrCreateDatabase("Artiles",MODE_PRIVATE,null);
+        articles.execSQL("CREATE TABLE IF NOT EXISTS articles(id INTEGER PRIMARY KEY,articleId INETGER,title VARCHAR,content VARCHAR)");
+
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, titles);
         ListView listView = findViewById(R.id.listView);
         listView.setAdapter(arrayAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),ArticleActivity.class);
+                intent.putExtra("content",contents.get(position));
+                startActivity(intent);
+            }
+        });
+
+        UpdateListView();
         DownloadTask task = new DownloadTask();
         try {
             task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+    public void UpdateListView(){
+        Cursor c = articles.rawQuery("SELECT * FROM articles",null);
+        int titleIndex = c.getColumnIndex("title");
+        int contentIndex = c.getColumnIndex("content");
+        if(c.moveToFirst()){
+            titles.clear();
+            contents.clear();
+            do{
+                titles.add(c.getString(titleIndex));
+                contents.add(c.getString(contentIndex));
+            }while(c.moveToNext());
+        }
+
+        arrayAdapter.notifyDataSetChanged();
     }
     public class DownloadTask extends AsyncTask<String,Void,String>{
         @Override
@@ -94,6 +129,13 @@ public class MainActivity extends AppCompatActivity {
                             data = reader.read();
                         }
                         Log.i("HTML",content);
+                        // add the details to the database
+                        String sql = "INSERT INTO articles (articleId,title,content) VALUES(?, ?, ?)";
+                        SQLiteStatement statement = articles.compileStatement(sql);
+                        statement.bindString(1,articleId); // index of column
+                        statement.bindString(2,title);
+                        statement.bindString(3,content);
+                        statement.execute();
 
                     }
                 }
@@ -105,6 +147,12 @@ public class MainActivity extends AppCompatActivity {
 
 
             return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            UpdateListView();
         }
     }
 }
